98284e93f27eb35bcc125d2c102df8e33961e0b7
djun100 <djun100@qq.com>
2017-01-22 14:23:45 +0800

    a

--- app/src/main/java/com/recker/flygallery/MainActivity.java
+++ app/src/main/java/com/recker/flygallery/MainActivity.java
@@ -1,19 +1,23 @@
 package com.recker.flygallery;
 
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
+import com.cy.DataStructure.UtilArray;
+import com.cy.DataStructure.UtilList;
+import com.pacific.adapter.PagerAdapterHelper;
+import com.pacific.adapter.ViewPagerAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ClipViewPagerLayout mClipViewPagerLL;
-
+    private Integer[] images = {R.drawable.img_1, R.drawable.img_2,
+            R.drawable.img_3, R.drawable.img_4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,38 +25,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mClipViewPagerLL= (ClipViewPagerLayout) findViewById(R.id.mClipViewPagerLL);
-        mClipViewPagerLL.mViewPager.setAdapter(new MyPagerAdapter());
-    }
 
+        ArrayList<Integer> beans= (ArrayList<Integer>) UtilList.convert_arrayToList(images);
 
-    private class MyPagerAdapter extends PagerAdapter {
-
-        private int[] images = {R.drawable.img_1, R.drawable.img_2,
-                R.drawable.img_3, R.drawable.img_4};
-
-        @Override
-        public int getCount() {
-            return images.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            ImageView imageView = new ImageView(MainActivity.this);
-            imageView.setImageResource(images[position]);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            container.addView(imageView);
-
-            return imageView;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((ImageView) object);
-        }
+        mClipViewPagerLL.mViewPager.setAdapter(new ViewPagerAdapter<Integer>(this,beans,R.layout.item) {
+            @Override
+            protected void convert(PagerAdapterHelper helper, Integer item) {
+                helper.setImageResource(R.id.imageView,item);
+            }
+        });
     }
 }
